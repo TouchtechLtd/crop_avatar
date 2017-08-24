3bef9416e83fb6db55baca0cc8e18ce4aedcad07
alex-zige <alex.zige@gmail.com>
2017-08-25 10:27:06 +1200

    Allow configurable for default missing path

--- app/models/concerns/cropable_avatar.rb
+++ app/models/concerns/cropable_avatar.rb
@@ -4,8 +4,8 @@ module CropableAvatar
     extend ActiveSupport::Concern
 
     included do
-        has_attached_file :avatar, 
-            styles: { medium: "300x300>", thumb: "100x100>" }, 
+        has_attached_file :avatar,
+            styles: { medium: "300x300>", thumb: "100x100>" },
             default_url: :gravatar_url,
             processors: [:avatar_cropper]
         validates_attachment_content_type :avatar, content_type: ['image/jpeg', 'image/jpeg', 'image/png']
@@ -20,7 +20,11 @@ module CropableAvatar
     private
     def gravatar_url
         hash = Digest::MD5.hexdigest(try(:email) || name&.parameterize || 'Unknown')
-        fallback_url = Rails.env.development? ? "mm" : "http://#{Rails.application.routes.default_url_options[:host]}/images/default_logo.jpg"
+        if CropAvatar.gravatar_default_missing_image_enabled
+            fallback_url =  "mm"
+        else
+            fallback_url = "http://#{Rails.application.routes.default_url_options[:host]}/#{CropAvatar.default_missing_image_path}"
+        end
         uri = "https://secure.gravatar.com/avatar/#{hash}?s=200&d=#{fallback_url}"
         uri.class.class_eval { def escape; self; end }
         uri
--- lib/crop_avatar.rb
+++ lib/crop_avatar.rb
@@ -2,4 +2,15 @@ require "crop_avatar/version"
 
 module CropAvatar
   class Engine < ::Rails::Engine; end
+
+  mattr_accessor :gravatar_default_missing_image_enabled
+  @@use_default_missing_image = true
+
+  mattr_accessor :default_missing_image_path
+  @@default_missing_image_path = "images/default_logo.jpg"
+
+  def self.setup
+    yield self
+  end
+
 end
