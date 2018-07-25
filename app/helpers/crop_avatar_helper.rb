800d90019020425f962e7e13fd545e6c90e5aced
Jesse Hills <jesse@springload.co.nz>
2018-07-25 16:16:04 +1200

    Change default resize value for form helper

--- app/helpers/crop_avatar_helper.rb
+++ app/helpers/crop_avatar_helper.rb
@@ -6,7 +6,7 @@ module CropAvatarHelper
     content_tag :div, id: 'avatar-image' do
       attr = "#{method}_crop"
       concat hidden_field_tag field_name(object, attr, options[:index]), 0, id: attr
-      concat "<div class='cropit-preview' data-url='#{object.cropped_avatar_variant(resize: '250x250')}'></div>".html_safe
+      concat "<div class='cropit-preview' data-url='#{object.cropped_avatar_variant(resize: '300x300')}'></div>".html_safe
       concat file_field_tag field_name(object, method, options[:index]), class: 'avatar-upload cropit-image-input', accept: 'image/*'
       concat text_field_tag 'file-path', nil, class: 'file-path', placeholder: "<%= t('.click_to_upload') %>", readonly: true
       concat "<script type='text/javascript'>cropAvatarSetup()</script>".html_safe
