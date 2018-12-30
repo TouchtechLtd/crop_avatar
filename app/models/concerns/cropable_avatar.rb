2f3e98b1b8fbb8dd979b557b7308d021dd5a981d
alex-zige <alex.zige@gmail.com>
2018-12-31 00:28:55 +1300

    Restore without variable check

--- app/models/concerns/cropable_avatar.rb
+++ app/models/concerns/cropable_avatar.rb
@@ -6,9 +6,9 @@ module CropableAvatar
     end
 
     def cropped_avatar_variant(**kwargs)
-        return gravatar_url if !avatar.attached? && !avatar.variable?
+        return gravatar_url unless avatar.attached?
         if !avatar_crop.blank?
-            avatar.variant(crop: avatar_crop, **kwargs) 
+            avatar.variant(crop: avatar_crop, **kwargs)
         else
             avatar.variant(**kwargs)
         end
